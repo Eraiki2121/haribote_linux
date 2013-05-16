844e74e3f3e474cee4b7603ede545aa05bcdda0e
Masahiro Yamada <masahiro@zoe.sesame>
2013-05-16 21:58:33 +0900

    modify harib01f

--- 04_day/harib01f/naskfunc.s
+++ 04_day/harib01f/naskfunc.s
@@ -6,72 +6,73 @@ io_hlt: # void io_hlt(void);
 	hlt
 	ret
 
-.global 
+.global io_cli
 io_cli:	# void io_cli(void);
 	cli
 	ret
 
-.global 
+.global io_sti
 io_sti:	# void io_sti(void);
 	sti
 	ret
 
-.global 
+.global io_stihlt
 io_stihlt: # void io_stihlt(void);
 	sti
 	hlt
 	ret
 
-.global 
+.global io_in8
 io_in8:	# int io_in8(int port);
-	mov 4(%esp), %edx		# port
-	mov $0, %eax
-	in %dx, %al
+	mov	4(%esp), %edx		# port
+	mov	$0, %eax
+	in	%dx, %al
 	ret
 
-.global 
-io_in16:	; int io_in16(int port);
-		MOV		EDX,[ESP+4]		; port
-		MOV		EAX,0
-		IN		AX,DX
-		RET
+.global io_in16
+io_in16: # int io_in16(int port);
+	mov	4(%esp), %edx		# port
+	mov	$0, %eax
+	in	%dx, %ax
+	ret
 
-.global
-io_in32:	; int io_in32(int port);
-		MOV		EDX,[ESP+4]		; port
-		IN		EAX,DX
-		RET
+.global io_in32
+io_in32: # int io_in32(int port);
+	mov	4(%esp), %edx		# port
+	mov	$0, %eax
+	in	%dx, %eax
+	ret
 
-.global
-io_out8:	; void io_out8(int port, int data);
-		MOV		EDX,[ESP+4]		; port
-		MOV		AL,[ESP+8]		; data
-		OUT		DX,AL
-		RET
+.global io_out8
+io_out8: # void io_out8(int port, int data)
+	mov	4(%esp), %edx		# port
+	mov	8(%esp), %eax		# data
+	out	%al, %dx
+	ret
 
-.global
-io_out16:	; void io_out16(int port, int data);
-		MOV		EDX,[ESP+4]		; port
-		MOV		EAX,[ESP+8]		; data
-		OUT		DX,AX
-		RET
+.global io_out16
+io_out16:	# void io_out16(int port, int data);
+	mov	4(%esp), %edx		# port
+	mov	8(%esp), %eax		# data
+	out	%ax, %dx
+	ret
 
-.global
-io_out32:	; void io_out32(int port, int data);
-		MOV		EDX,[ESP+4]		; port
-		MOV		EAX,[ESP+8]		; data
-		OUT		DX,EAX
-		RET
+.global io_out32
+io_out32:	# void io_out32(int port, int data);
+	mov	4(%esp), %edx		# port
+	mov	8(%esp), %eax		# data
+	out	%eax, %dx
+	ret
 
-.global
-io_load_eflags:	; int io_load_eflags(void);
-		PUSHFD		; PUSH EFLAGS という意味
-		POP		EAX
-		RET
+.global io_load_eflags
+io_load_eflags:	# int io_load_eflags(void);
+	pushf		# PUSH EFLAGS
+	pop	%eax
+	ret
 
-.global
-io_store_eflags:	; void io_store_eflags(int eflags);
-		MOV		EAX,[ESP+4]
-		PUSH	EAX
-		POPFD		; POP EFLAGS という意味
-		RET
+.global io_store_eflags
+io_store_eflags:	# void io_store_eflags(int eflags);
+	mov	4(%eax), %eax
+	push	%eax
+	popf		# POP EFLAGS
+	ret
